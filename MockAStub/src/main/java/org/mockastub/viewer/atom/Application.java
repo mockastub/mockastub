474b29cbfdd4dcb6497352adc70f6eb5581573e6
ClausElkjaerChristensen <claus@elkjaer-christensen.dk>
2016-01-14 21:43:28 +0100

    more atom

--- MockAStub/src/main/java/org/mockastub/viewer/atom/Application.java
+++ MockAStub/src/main/java/org/mockastub/viewer/atom/Application.java
@@ -1,24 +1,19 @@
 package org.mockastub.viewer.atom;
-	import java.util.Arrays;
 
-	import org.springframework.boot.SpringApplication;
-	import org.springframework.boot.autoconfigure.SpringBootApplication;
-	import org.springframework.context.ApplicationContext;
+import java.util.Arrays;
 
-	@SpringBootApplication
-	public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-	    public static void main(String[] args) {
-	        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+@SpringBootApplication
+public class Application {
 
-	        System.out.println("Let's inspect the beans provided by Spring Boot:");
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-	        String[] beanNames = ctx.getBeanDefinitionNames();
-	        Arrays.sort(beanNames);
-	        for (String beanName : beanNames) {
-	            System.out.println(beanName);
-	        }
-	    }
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
 
+    }
 
 }
--- MockAStub/src/main/java/org/mockastub/viewer/atom/MessageProviderServlet.java
+++ MockAStub/src/main/java/org/mockastub/viewer/atom/MessageProviderServlet.java
@@ -7,17 +7,21 @@ import org.apache.abdera.protocol.server.Provider;
 import org.apache.abdera.protocol.server.impl.DefaultProvider;
 import org.apache.abdera.protocol.server.impl.SimpleWorkspaceInfo;
 import org.apache.abdera.protocol.server.servlet.AbderaServlet;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
  * @author claus
  *
  */
+@Controller
 public class MessageProviderServlet extends AbderaServlet {
 
 	/**
 	 * 
 	 */
+	@RequestMapping("/")
     protected Provider createProvider() {
          MessageInboundCollectionAdapter ca = new MessageInboundCollectionAdapter();
          ca.setHref("message");
