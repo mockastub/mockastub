c842c6be0d687293e343c7a21dc38e3dbe8f113c
ClausElkjaerChristensen <claus@elkjaer-christensen.dk>
2016-01-15 09:39:43 +0100

    resolved gradle dependency

--- MockAStub/src/main/java/org/mockastub/viewer/atom/Application.java
+++ MockAStub/src/main/java/org/mockastub/viewer/atom/Application.java
@@ -1,13 +1,20 @@
 package org.mockastub.viewer.atom;
 
-import java.util.Arrays;
+import javax.jms.ConnectionFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	
+    @Bean // Strictly speaking this bean is not necessary as boot creates a default
+    ServletRegistrationBean myServletRegistrationBean(ConnectionFactory connectionFactory) {
+    	return new ServletRegistrationBean(new MessageProviderServlet(), "/viewer/*");
+    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
